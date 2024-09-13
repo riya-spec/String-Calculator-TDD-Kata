0d4202a2e922d1946dfb2dd444fec05ee17769d3
Riya <71124402+riya-spec@users.noreply.github.com>
2024-09-13 09:20:00 +0000

    Handle unknown amount of numbers

--- string-calculator.js
+++ string-calculator.js
@@ -5,10 +5,6 @@ function add(numbers) {
   
     const numList = numbers.split(','); // Split by comma
   
-    if (numList.length === 1) {
-      return parseInt(numbers, 10); // Return the single number
-    }
-  
-    return parseInt(numList[0], 10) + parseInt(numList[1], 10); // Sum of two numbers
+    return numList.reduce((sum, num) => sum + parseInt(num, 10), 0); // Sum all numbers
   }
   
