5f0bbc54b4cc0952a06957fd41007489b824b818
Riya <71124402+riya-spec@users.noreply.github.com>
2024-09-13 09:18:05 +0000

    Handle a string with two number

--- string-calculator.js
+++ string-calculator.js
@@ -3,7 +3,12 @@ function add(numbers) {
       return 0;
     }
   
-    return parseInt(numbers, 10); // Return the number itself for a single number
-  }
+    const numList = numbers.split(','); // Split by comma
   
+    if (numList.length === 1) {
+      return parseInt(numbers, 10); // Return the single number
+    }
+  
+    return parseInt(numList[0], 10) + parseInt(numList[1], 10); // Sum of two numbers
+  }
   
