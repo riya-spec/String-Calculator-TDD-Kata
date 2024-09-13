2453f5ce4e457400d1666f3628697dd5a0cc18d8
Riya <71124402+riya-spec@users.noreply.github.com>
2024-09-13 09:15:56 +0000

    To handle single number

--- string-calculator.js
+++ string-calculator.js
@@ -1,8 +1,9 @@
 function add(numbers) {
     if (numbers === "") {
-      return 0; // Return 0 for an empty string
+      return 0;
     }
+  
+    return parseInt(numbers, 10); // Return the number itself for a single number
   }
   
-  module.exports = add;
   
